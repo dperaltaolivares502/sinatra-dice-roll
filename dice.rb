8b6aba755cf2c0b649582e31795d3a5a80446b8d
dperaltaolivares502 <143421071+dperaltaolivares502@users.noreply.github.com>
2024-01-30 15:34:56 +0000

    hi103

--- dice.rb
+++ dice.rb
@@ -34,7 +34,7 @@ get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome10 = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
